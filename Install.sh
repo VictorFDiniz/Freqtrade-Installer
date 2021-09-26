79b2e61d6cb1435b87cb76037c8b13e1b7d2ef97
Victor Ferreira Diniz <86570043+VictorFDiniz@users.noreply.github.com>
2021-09-26 17:16:20 -0300

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-sudo apt-get update && apt-get upgrade -y
-sudo apt-get install build-essential -y
+  [[ "$EUID" -ne 0 ]] && echo "Please, run this as root" && exit 1
+
+  apt-get update && apt-get upgrade -y
+  apt-get install build-essential -y
 
 # Python
 
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt-get update
-sudo apt-get install python3.8
+  apt-get install software-properties-common
+  add-apt-repository ppa:deadsnakes/ppa
+  apt-get update
+  apt-get install python3.8
 
 # install packages
 
-sudo apt install -y python3-pip python3-venv python3-dev python3-pandas git
+  apt install -y python3-pip python3-venv python3-dev python3-pandas git
 
 # Download `develop` branch of freqtrade repository
 
-git clone https://github.com/freqtrade/freqtrade.git
+  git clone https://github.com/freqtrade/freqtrade.git
 
-cd freqtrade
-./setup.sh -i
-source ./.env/bin/activate
+  cd freqtrade
+  ./setup.sh -i
+  source ./.env/bin/activate
