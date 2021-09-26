a71b88cfc378db44df8c058d5f6861786d9188fc
Victor Ferreira Diniz <86570043+VictorFDiniz@users.noreply.github.com>
2021-09-26 12:43:46 -0300

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+apt-get update && apt-get upgrade -y
+apt-get install build-essential -y
+
+# Python
+
+apt-get install software-properties-common
+add-apt-repository ppa:deadsnakes/ppa
+apt-get update
+apt-get install python3.8
+
+# install packages
+
+sudo apt install -y python3-pip python3-venv python3-dev python3-pandas git
+
+# Download `develop` branch of freqtrade repository
+
+git clone https://github.com/freqtrade/freqtrade.git
+
+cd freqtrade
+./setup.sh -i
+source ./.env/bin/activate
