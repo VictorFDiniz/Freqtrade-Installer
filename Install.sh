dfc9a4f864c354552416f7ccbe00ce03c1088ff2
Victor Ferreira Diniz <86570043+VictorFDiniz@users.noreply.github.com>
2021-10-17 15:42:58 -0300

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -22,7 +22,7 @@
 
 # install packages
 
-  apt install -y python3-pip python3-venv python3-dev python3-pandas screen
+  apt-get install -y python3-pip python3-venv python3-dev python3-pandas screen
   
 # fail2ban
 
